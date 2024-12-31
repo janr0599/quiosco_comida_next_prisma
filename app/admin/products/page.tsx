31578a9056ec477564b9aaee7205d66968eac33d
janr0599 <javiernr0599@gmail.com>
2024-12-31 19:14:50 -0400

    Admin panel navigation links and styles

---
+++ app/admin/products/page.tsx
@@ -0,0 +1,13 @@
+import Heading from "@/components/ui/Heading";
+
+function ProductsPage() {
+    console.log("Desde el servidor ");
+
+    return (
+        <>
+            <Heading>Administrar Productos</Heading>
+        </>
+    );
+}
+
+export default ProductsPage;
--- app/products/page.tsx
+++
@@ -1,7 +0,0 @@
-function ProductsPage() {
-    console.log("Desde el servidor ");
-
-    return <h1>ProductsPage</h1>;
-}
-
-export default ProductsPage;
