c015dfcb09a0fc5c235922f1d7aea4fef074ced9
janr0599 <javiernr0599@gmail.com>
2025-01-04 17:27:19 -0400

    Add products table in admin panel

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,11 +1,18 @@
+import ProductsTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
 
-function ProductsPage() {
-    console.log("Desde el servidor ");
+async function getProducts() {
+    const products = await prisma.product.findMany();
+    return products;
+}
 
+async function ProductsPage() {
+    const products = await getProducts();
     return (
         <>
             <Heading>Administrar Productos</Heading>
+            <ProductsTable products={products} />
         </>
     );
 }
