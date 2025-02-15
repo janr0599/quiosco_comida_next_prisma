c5af90a71ce202dae19550375419a902d6bfd615
janr0599 <javiernr0599@gmail.com>
2025-02-15 11:24:30 -0400

    Add button to manually revalidate ordes path in the admin dashboard

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,6 +1,7 @@
 import OrderCard from "@/components/order/OrderCard";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
+import { revalidatePath } from "next/cache";
 
 async function getPendingOrders() {
     const orders = await prisma.order.findMany({
@@ -20,9 +21,20 @@ async function getPendingOrders() {
 
 async function OrdersPage() {
     const orders = await getPendingOrders();
+    const refreshOrders = async () => {
+        "use server";
+        revalidatePath("/admin/orders");
+    };
     return (
         <>
             <Heading>Administrar Ordenes</Heading>
+            <form action={refreshOrders}>
+                <input
+                    type="submit"
+                    value="Actualizar Ordenes"
+                    className="bg-amber-400 w-full lg:w-auto text-xl px-10 py-3 text-center font-bold cursor-pointer rounded-md"
+                />
+            </form>
             {orders.length ? (
                 <div className="grid grid-cols-1 gap-5 mt-5 md:grid-cols-2 lg:grid-cols-3">
                     {orders.map((order) => (
