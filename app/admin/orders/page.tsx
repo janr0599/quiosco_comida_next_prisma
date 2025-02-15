4cc48c67584efb9af0cf1f10b20328744bea379e
janr0599 <javiernr0599@gmail.com>
2025-02-15 15:47:56 -0400

    Add swr to fecth orders and created api endpoint

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,51 +1,42 @@
+"use client";
+import useSWR from "swr";
 import OrderCard from "@/components/order/OrderCard";
 import Heading from "@/components/ui/Heading";
-import { prisma } from "@/src/lib/prisma";
-import { revalidatePath } from "next/cache";
+import { OrderWithProducts } from "@/src/types";
 
-async function getPendingOrders() {
-    const orders = await prisma.order.findMany({
-        where: {
-            status: false,
-        },
-        include: {
-            orderProducts: {
-                include: {
-                    product: true,
-                },
-            },
-        },
-    });
-    return orders;
-}
+function OrdersPage() {
+    const url = "/admin/orders/api";
+    const fetcher = () =>
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => data.orders);
 
-async function OrdersPage() {
-    const orders = await getPendingOrders();
-    const refreshOrders = async () => {
-        "use server";
-        revalidatePath("/admin/orders");
-    };
-    return (
-        <>
-            <Heading>Administrar Ordenes</Heading>
-            <form action={refreshOrders}>
-                <input
-                    type="submit"
-                    value="Actualizar Ordenes"
-                    className="bg-amber-400 w-full lg:w-auto text-xl px-10 py-3 text-center font-bold cursor-pointer rounded-md"
-                />
-            </form>
-            {orders.length ? (
-                <div className="grid grid-cols-1 gap-5 mt-5 md:grid-cols-2 lg:grid-cols-3">
-                    {orders.map((order) => (
-                        <OrderCard key={order.id} order={order} />
-                    ))}
-                </div>
-            ) : (
-                <p className="text-center">No hay ordenes pendientes</p>
-            )}
-        </>
+    const { data, error, isLoading } = useSWR<OrderWithProducts[]>(
+        url,
+        fetcher,
+        {
+            refreshInterval: 1000 * 60,
+        }
     );
+
+    if (isLoading) return <p>Cargando...</p>;
+
+    if (data)
+        return (
+            <>
+                <Heading>Administrar Ordenes</Heading>
+
+                {data.length ? (
+                    <div className="grid grid-cols-1 gap-5 mt-5 md:grid-cols-2 lg:grid-cols-3">
+                        {data.map((order) => (
+                            <OrderCard key={order.id} order={order} />
+                        ))}
+                    </div>
+                ) : (
+                    <p className="text-center">No hay ordenes pendientes</p>
+                )}
+            </>
+        );
 }
 
 export default OrdersPage;
